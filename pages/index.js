6853bb96c94cd49d0fce492dfa2c86589d3b856e
Enrique Torreblanca <enriquetchat@gmail.com>
2023-04-16 15:49:59 -0700

    Add pages/index.js

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import CustomComponent from './CustomComponent';
+
+function App() {
+  return (
+    <div>
+      <CustomComponent />
+    </div>
+  );
+}
+
+export default App;
